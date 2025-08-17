e090d50068af72613e8e54bc98675265a8d189f6
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-08-17 13:15:52 -0300

    Armando entorno de trabajo Arqui

---
+++ Assemblerx86_64/hello.asm
@@ -0,0 +1,16 @@
+section .text
+GLOBAL _start
+_start:
+mov ecx, cadena ; Puntero a la cadena
+mov edx, longitud ; Largo de la cadena
+mov ebx, 1 ; FileDescriptor (STDOUT)
+mov eax, 4 ; ID del Syscall WRITE
+int 80h ; Ejecución de la llamada
+mov eax, 1 ; ID del Syscall EXIT
+mov ebx, 0 ; Valor de Retorno
+int 80h ; Ejecución de la llamada
+section .data
+cadena db "Hola Mundo!!", 10 ;"Hola Mundo!!\n"
+longitud equ $-cadena
+section .bss
+placeholder resb 10
