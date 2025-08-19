a183d36f145d6c913f8c7c3c0e686694585aeb8c
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-08-19 14:10:39 -0300

    Helloworld v2 con funciones

--- Assemblerx86_64/src/Hello/my_hello_v2.asm
+++ Assemblerx86_64/src/Hello/my_hello_v2.asm
@@ -1,14 +1,27 @@
-_start
-;; Inicio del programa
-
 section .text
 ;; Seccion No Editable
 ;; Seccion Ejecutable
+
+;; Si o si hay que poner GLOBAL _start
+;; porque sino no encuentra el _start y hace un default
+;; que seria comportamiento "indefinido"
+GLOBAL _start
+
+_start:
+    ;; Inicio del programa
     mov edx, length
     mov ecx, to_print
     call print
+    mov eax, 1
+    mov ebx, 0
+    int 80h
 
-.print
+;; duda / doubt
+;; Cual seria la diferencia entre hacer esta declaracion:
+;; .print:
+;; y la que hice en el codigo?
+print:
+    ;; Funcion que pertenece a la seccion .text
     push ebp
     mov ebp, esp
     mov eax, 4 ;; Syscall ID de write: 4
@@ -25,7 +38,7 @@ section .data
 
     ;; Necesitamos pasarle el codigo int que representa
     ;; el ASCII del \n (New line)
-    to_print "Hello world!", 10
+    to_print db "Hello world!", 10
 
     ;; Forma de definir una variable, se usa la keyword: equ
     length equ $ - to_print
