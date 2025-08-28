36baa7d0493238fa2daa1c59a96e4916cc6cf8e7
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-08-28 20:38:10 -0300

    Factorial casi terminado

--- Assemblerx86_64/src/factorial.asm
+++ Assemblerx86_64/src/factorial.asm
@@ -5,6 +5,8 @@ global factorial
 ;; Dado un número n, imprimir su factorial.
 ;; Tenga cuidado con los argumentos de la función.
 ;; Recibo @n por stack
+;; Si es negativo o 0, retorna 1
+
 factorial:
     ;; Armado de stackframe
     push ebp
@@ -15,14 +17,23 @@ factorial:
     ;; Guardo copia de la copia, para usar de multiplicador
     mov ebx, eax
     dec ebx
+    cmp eax, 1 ;; Comparo el parametro ingresado con 1
+    jle zero_case ;; Si el parametro que me pasan es menor o igual a 1, voy al caso de zero
 
 .multiplication_loop:
-    mul ebx
+    mul ebx ;; Multiplica eax con ebx y deja la respuesta en eax
     dec ebx
     jnz .multiplication_loop
 
 .print_number:
     
+
+end:
+
+
+zero_case:
+    mov eax, 1
+    jmp end
 
 section .data
 
